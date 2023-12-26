0bd5698cd2f67337e3e51d75b3a0a87d28a1d244
akshya <akshya.nanda03@gmail.com>
2023-12-26 09:49:21 +0530

    till JWT in auth

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
+require("./config/database").connect();
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('./models/user');
 
@@ -11,6 +14,7 @@ app.get("/", (req, res)=>{
 });
 
 app.post("/register", async(req, res)=>{
+    try {
     const {firstname, lastname, email, password} = req.body;
 
     if (!(email && firstname && lastname && password)){
@@ -23,6 +27,34 @@ app.post("/register", async(req, res)=>{
     if (existingUser)
         res.status(401).send('User already exists');
 
+    //encryption
+    const myEncPassword = await bcrypt.hash(password, 10);
+
+    // after creating a User, mongoDB/mongoose will assign a unique obj id which we can access with the variable "user"
+    const user = await User.create({
+        firstname,
+        lastname,
+        email: email.toLowercase(),
+        password: myEncPassword
+    });
+
+    //token
+    const token = jwt.sign(
+        {user_id: user._id, email},
+        process.env.SECRET_KEY,
+        {
+            expiresIn:"2h"
+        }
+    );
+    user.token = token;
+
+    //update or not in DB
+
+    //TODO: handle password situation
+    res.status(201).json(user);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 // instead of using the LISTEN code here, we will export it to index.js coz this will get bigger with middlewares, routes
---
+++ config/database.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const {MONGODB_URL} = process.env;
+
+exports.connect = () => {
+    //PROMISE....so then().catch()
+    mongoose.connect(MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(
+        console.log(`DB CONNECTED SUCCESSFULLY`)
+    )
+    .catch((error)=>{
+        console.log(`DB CONNECTION FAILED`);
+        console.log(error);
+        process.exit(1);
+    });
+};
+
